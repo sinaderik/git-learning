0afd46b2660dbd9a76509a8654e5224d25acbe33
Sina <sinaderik7@gmail.com>
2024-10-10 15:37:24 +0330

    undo the changes

--- git practice 1/hello.js
+++ git practice 1/hello.js
@@ -1,5 +1,3 @@
 console.log('Hello, git')
 console.log('get result function')
 console.log('map over an array + modification added')
-console.log('add get sum function')
-console.log('git revert command')
